0b6886efea4dd877d5c55e555678887d18471e41
Rachmanlazu <rachmanlazuardi.9a@gmail.com>
2017-12-27 23:17:37 +0700

    yang pertama

---
+++ Assets/assets/scripts/ApiPeluru.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApiPeluru : MonoBehaviour {
+
+	public float waktuTiapPeluru = 0.15f;
+	public GameObject projectile;
+
+	float nextPeluru;
+
+	// Use this for initialization
+	void Awake () {
+		nextPeluru = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerController pemain = transform.root.GetComponent<playerController> ();
+
+		if (Input.GetAxisRaw ("Fire1") > 0 && nextPeluru < Time.time) {
+			nextPeluru = Time.time + waktuTiapPeluru;
+			Vector3 rot;
+			if (pemain.GetFacing () == -1f) {
+				rot = new Vector3 (0, -90, 0);
+			} else
+				rot = new Vector3 (0, 90, 0);
+
+			Instantiate (projectile, transform.position, Quaternion.Euler (rot));
+		}
+	}
+}
---
+++ Assets/assets/scripts/PlayerHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public float fullHealth;
+	float currentHealth;
+
+	public GameObject playerDeathFX;
+
+	// Use this for initialization
+	void Start () {
+		currentHealth = fullHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void addDamage (float damage) {
+		currentHealth -= damage;
+		if (currentHealth <= 0) {
+			makeDead ();
+		}
+	}
+
+	public void makeDead(){
+		Instantiate(playerDeathFX, transform.position, Quaternion.Euler(new Vector3(-90,0,0)));
+		Destroy (gameObject); 
+	}
+}
